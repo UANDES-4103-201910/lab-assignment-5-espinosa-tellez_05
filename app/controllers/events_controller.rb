40eeeb08e06f3d31d18ce6612e4fdd9beb9f1b36
MEspinosaGH <mespinosa3@miuandes.cl>
2019-04-10 17:58:11 -0400

    tellexxxx no hizo nada!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,33 @@
 class EventsController < ApplicationController
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render "new"
+    end
+  end
+
+  def new
+    @event = Event.new   
+  end
+
+  def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update!(event_params)
+    redirect_to @event
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
   end
 
   def index
@@ -25,5 +47,9 @@ class EventsController < ApplicationController
       format.xml  { render xml: @event }
       format.json { render json: @event }
     end
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :start_date, :event_vanue)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
-
   def update
   	@user = User.find(params[:id])
     @user.update!(user_params)
