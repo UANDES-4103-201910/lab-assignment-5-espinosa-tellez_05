fa1567733300254a33782b2350c42189ecee3f7b
jatellezs <jatellez@miuandes.cl>
2019-04-10 14:13:41 -0400

    oh my god

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,11 +1,44 @@
 class TicketsController < ApplicationController
-  def create
+ def create
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.save
+      redirect_to @ticket
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render "new"
+    end
   end
 
+  def new
+    @ticket = Ticket.new   
+  end
+
+  def edit
+    @ticket = Ticket.find(params[:id])
+  end
+
+
   def update
+    @ticket = Ticket.find(params[:id])
+    @ticket.update!(ticket_params)
+    redirect_to @ticket
   end
 
   def destroy
+    @ticket = Tciket.find(params[:id])
+    @ticket.destroy
+    redirect_to tickets_path
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+ 
+    respond_to do |format|
+      format.html
+      format.xml  { render xml: @ticket }
+      format.json { render json: @ticket }
+    end
   end
 
   def index
@@ -17,13 +50,7 @@ class TicketsController < ApplicationController
     end
   end
 
-  def show
-    @ticket = Ticket.find(params[:id])
- 
-    respond_to do |format|
-      format.html
-      format.xml  { render xml: @ticket }
-      format.json { render json: @ticket }
-    end
+  def ticket_params
+    params.require(:ticket).permit(:ticket_order, :order)
   end
 end
